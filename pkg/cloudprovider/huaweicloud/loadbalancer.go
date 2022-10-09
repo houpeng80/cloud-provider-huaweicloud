08d340690b2efa3ba7cffcc6e52c92a8e1158b0d
houpeng <hp_monkey@163.com>
2022-10-09 14:43:04 +0800

    add logs

--- pkg/cloudprovider/huaweicloud/loadbalancer.go
+++ pkg/cloudprovider/huaweicloud/loadbalancer.go
@@ -657,6 +657,7 @@ func getNodeSubnetID(hc *HuaweiCloud, node corev1.Node) (string, error) {
 	if ind := strings.LastIndex(instanceID, "/"); ind >= 0 {
 		instanceID = instanceID[(ind + 1):]
 	}
+	klog.V(1).Infof("node instanceID: %s", instanceID)
 
 	interfaces, err := hc.computeService.ListInterfaces(instanceID)
 	if err != nil {
